650587c592efdd2f8f82cd7cac4849a00b9027e6
kaiobrito <kaio.a7x@gmail.com>
2020-05-09 09:59:17 -0300

    Initial Commit

---
+++ SwiftUIAnimationsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAnimationsDemo
+//
+//  Created by Kaio Brito on 09/05/20.
+//  Copyright © 2020 Kaio Brito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
