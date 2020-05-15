fb4d9f761dc5bd48046cc96348df2abf49dbf1ca
kaiobrito <kaio.a7x@gmail.com>
2020-05-15 16:10:09 -0300

    Use mwl colours

---
+++ SwiftUIAnimationsDemo/Color+extension.swift
@@ -0,0 +1,18 @@
+//
+//  Color+extension.swift
+//  SwiftUIAnimationsDemo
+//
+//  Created by Kaio Brito on 15/05/20.
+//  Copyright © 2020 Kaio Brito. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let heartRed = Color("heartRed")
+    static let outlineBlack = Color("outlineBlack")
+    static let strangeBlue = Color("strangeBlue")
+    static let darkGray = Color("darkGray")
+    static let lightGray = Color("lightGray")
+    static let secondaryYellow = Color("secondaryYellow")
+}
--- SwiftUIAnimationsDemo/SignInView.swift
+++ SwiftUIAnimationsDemo/SignInView.swift
@@ -14,40 +14,92 @@ struct SignInView: View {
 
     var body: some View {
         ZStack {
-            ZStack {
+            Color.heartRed.edgesIgnoringSafeArea(.all)
+
+            VStack {
+                Spacer()
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .zIndex(999)
+                Spacer()
+                
                 VStack {
-                    Text("Welcome")
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Text("Continue")
-                        Image(systemName: "arrow.right")
+                    HStack(alignment: .center) {
+                        Text("Log in").font(.headline).fontWeight(.semibold).foregroundColor(Color.heartRed)
+                        Text("to you account")
+                            .font(.subheadline)
+                            .foregroundColor(Color.gray)
                     }
-                    .padding(.horizontal)
-                    .foregroundColor(Color.white)
-                    .frame(height: 64)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.black)
-                    .onTapGesture(perform: self.submit)
-                }.background(Color.white)
+                    
+                    VStack {
+                        Spacer()
+                        TextInput(label: "Username", icon: "envelope.fill")
+                        VStack {
+                            TextInput(label: "Password", icon: "lock.fill")
+                            HStack {
+                                Spacer()
+                                Button (action: {}) {
+                                    Text("Forgot your password?")
+                                        .font(.subheadline)
+                                        .foregroundColor(Color.heartRed)
+                                        .multilineTextAlignment(.trailing)
+                                }
+                            }
+                        }
+                        Spacer()
+                    }.padding(.vertical)
+                    
+                    
+                    VStack {
+                        SubmitButton(text: "Sign In", onSubmit: self.submit)
+                        
+                        Button(action:{}) {
+                            HStack {
+                                Text("Don't have an account?")
+                                    .foregroundColor(Color.gray)
+                                Text("Sign Up")
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(Color.heartRed)
+                            }
+                        }
+                    }
+                }
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.white)
+                        .edgesIgnoringSafeArea(.bottom)
+                        .shadow(radius: 3)
+                )
+                .frame(maxHeight: 500)
+                .background(
+                    Image("busy")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity).offset(x: 0, y: -100),
+                    alignment: .top)
             }
             .blur(radius: isSubmitting ? 9 : 0.0)
-                VStack {
-                    Image(systemName: "person")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .padding()
-                        .background(
-                            Circle()
-                                .stroke(lineWidth: 3)
-                                .fill(LinearGradient(gradient: Gradient(colors: [Color.yellow, Color.black ,Color.red]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                                .rotationEffect(Angle(degrees: isSubmitting ? 360 + 90 : 90))
-                                .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
-                        )
-                    Text("Submitting")
-                }
+            
+            VStack {
+                Spacer()
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .padding()
+                    .foregroundColor(Color.darkGray)
+                    .background(
+                        Circle()
+                            .stroke(lineWidth: 3)
+                            .fill(LinearGradient(gradient: Gradient(colors: [Color.heartRed, Color.secondaryYellow ,Color.darkGray]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                            .rotationEffect(Angle(degrees: isSubmitting ? 360 + 90 : 90))
+                            .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                )
+                Text("Submitting")
+                Spacer()
+            }
             .opacity(isSubmitting ? 1.0 : 0.0)
         }.animation(.easeIn)
     }
@@ -55,7 +107,7 @@ struct SignInView: View {
     func submit() {
         self.isSubmitting = true
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             self.isSubmitting = false
             self.appState.isSignedIn = true
         }
@@ -64,6 +116,78 @@ struct SignInView: View {
 
 struct SignInView_Previews: PreviewProvider {
     static var previews: some View {
-        SignInView(appState: Binding<AppState>.constant(AppState()))
+        Group {
+            SignInView(appState: Binding<AppState>.constant(AppState()))
+                .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+                .previewDisplayName("iPhone SE")
+            SignInView(appState: Binding<AppState>.constant(AppState()))
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro"))
+                .previewDisplayName("iPhone 11 pro")
+        }
+    }
+}
+
+struct TextInput: View {
+    var label: String
+    var icon: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5.0) {
+            HStack {
+                Text(label)
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
+            .padding(.horizontal)
+            HStack {
+                HStack {
+                    Image(systemName: icon)
+                        .foregroundColor(Color.darkGray)
+                        .scaledToFit()
+                    Spacer()
+                    Rectangle()
+                        .fill(Color.darkGray)
+                        .frame(width: 1)
+                        .padding(.vertical)
+                }
+                .frame(width: 24)
+                
+                TextField(label, text: Binding<String>.constant(""))
+                
+            }
+            .frame(height: 52)
+            .padding(.horizontal)
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.white)
+                    .shadow(radius: 1)
+            )
+        }.frame(maxWidth: .infinity)
+    }
+}
+
+struct SubmitButton: View {
+    var text: String
+    var onSubmit: () -> Void
+    @State var isPressing = false
+
+    var body: some View {
+        HStack {
+            Text(text)
+        }
+        .foregroundColor(Color.white)
+        .frame(height: 64)
+        .frame(maxWidth: .infinity)
+        .background(Color.heartRed)
+        .cornerRadius(8)
+        .padding(.vertical)
+        .shadow(radius: 2)
+        .scaleEffect(isPressing ? 0.9 : 1.0)
+        .onTapGesture(perform: self.onSubmit)
+        .onLongPressGesture(minimumDuration: 1, maximumDistance: 32, pressing: {
+            self.isPressing = $0
+        }, perform: {
+            self.isPressing.toggle()
+        })
     }
 }
